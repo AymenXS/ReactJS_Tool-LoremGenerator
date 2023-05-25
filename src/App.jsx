0fa283c1ae49bc9132059b4d7845163ac6e8100b
AymenXS <aymenghaloua@gmail.com>
2023-05-25 22:39:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import data from './data';
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState(['']);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" action="" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Number: </label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+          min="1"
+          max="8"
+          step="1"
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+};
+export default App;
